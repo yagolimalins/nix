81a4f01b2ad4c80d2e2cd570862db368d85c337e
Yago Lima Lins <yago.lima.lins@protonmail.com>
2025-10-14 16:14:07 -0300

    feat: add custom keybindings

--- modules/dconf.nix
+++ modules/dconf.nix
@@ -5,5 +5,22 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+    
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+      next = ["<Super>period"];
+      play = ["<Super>semicolon"];
+      previous = ["<Super>comma"];
+      volume-down = ["<Super>bracketright"];
+      volume-up = ["<Super>bracketleft"];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Terminal";
+      command = "gnome-terminal";
+      binding = "<Super>Return";
+    };
   };
 }
