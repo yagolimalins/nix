aba05864c28696f76c96fa3aedf397c2849c93a8
Yago Lima Lins <yago.lima.lins@protonmail.com>
2025-10-15 14:54:13 -0300

    feat: add dconf settings for workspaces and and favorite apps

--- modules/dconf.nix
+++ modules/dconf.nix
@@ -5,16 +5,78 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
-    
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 6;
+    };
+
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "org.gnome.Nautilus.desktop"
+        "org.gnome.Terminal.desktop"
+        "firefox.desktop"
+        "code.desktop"
+        "dev.zed.Zed.desktop"
+        "dbeaver.desktop"
+        "github-desktop.desktop"
+        "spotify.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+      move-to-workspace-5 = [ "<Shift><Super>5" ];
+      move-to-workspace-6 = [ "<Shift><Super>6" ];
+      move-to-workspace-7 = [ "<Shift><Super>7" ];
+      move-to-workspace-8 = [ "<Shift><Super>8" ];
+      move-to-workspace-9 = [ "<Shift><Super>9" ];
+      move-to-workspace-10 = [ "<Shift><Super>0" ];
+
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>7" ];
+      switch-to-workspace-8 = [ "<Super>8" ];
+      switch-to-workspace-9 = [ "<Super>9" ];
+      switch-to-workspace-10 = [ "<Super>0" ];
+
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+      switch-to-application-5 = [ ];
+      switch-to-application-6 = [ ];
+      switch-to-application-7 = [ ];
+      switch-to-application-8 = [ ];
+      switch-to-application-9 = [ ];
+    };
+
+    "org/gnome/shell/keybindings" = {
+      show-screenshot-ui = [ "<Super>s" ];
+      toggle-quick-settings = [ ];
+    };
+
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
       ];
-      next = ["<Super>period"];
-      play = ["<Super>semicolon"];
-      previous = ["<Super>comma"];
-      volume-down = ["<Super>bracketright"];
-      volume-up = ["<Super>bracketleft"];
+      next = [ "<Super>period" ];
+      play = [ "<Super>semicolon" ];
+      previous = [ "<Super>comma" ];
+      volume-down = [ "<Super>bracketright" ];
+      volume-up = [ "<Super>bracketleft" ];
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
