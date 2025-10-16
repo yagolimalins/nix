b608ddc753462934e7ff4b584c6be966be159652
Yago Lima Lins <yago.lima.lins@protonmail.com>
2025-10-16 16:42:36 -0300

    fix: add backupFileExtension = null to flake.nix

--- flake.nix
+++ flake.nix
@@ -9,34 +9,44 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
 
-  let
-    system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
 
-    # List all host folders under ./hosts
-    hostDirs = builtins.attrNames (builtins.readDir ./hosts);
+      # List all host folders under ./hosts
+      hostDirs = builtins.attrNames (builtins.readDir ./hosts);
 
-    mkHost = hostName:
-      nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          ./hosts/${hostName}/hardware-configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.yago = import ./home.nix;
-          }
-        ];
-      };
+      mkHost =
+        hostName:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            ./hosts/${hostName}/hardware-configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = null;
+              home-manager.users.yago = import ./home.nix;
+            }
+          ];
+        };
 
-  in
-  {
-    nixosConfigurations = builtins.listToAttrs (map (hostName: {
-      name = hostName;
-      value = mkHost hostName;
-    }) hostDirs);
-  };
+    in
+    {
+      nixosConfigurations = builtins.listToAttrs (
+        map (hostName: {
+          name = hostName;
+          value = mkHost hostName;
+        }) hostDirs
+      );
+    };
 }
