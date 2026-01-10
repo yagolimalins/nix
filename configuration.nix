af7803b01b414f231725fe988da3ecfa820c277c
Yago Lima Lins <yago.lima.lins@protonmail.com>
2026-01-10 20:14:38 -0300

    fix: update deprecated configuration.nix option names

--- configuration.nix
+++ configuration.nix
@@ -98,8 +98,8 @@
 
   # X11 and GNOME
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   services.xserver.xkb.layout = "br";
   services.xserver.xkb.variant = "";
 
