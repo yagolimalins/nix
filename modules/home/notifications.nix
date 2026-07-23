17852b0fdc03c7a0c6a49d1b94c58a4580434d55
Yago Lima Lins <yago.lima.lins@protonmail.com>
2026-07-23 16:40:19 -0300

    fix: adjust mako settings

--- modules/home/notifications.nix
+++ modules/home/notifications.nix
@@ -19,6 +19,13 @@
       padding             = "10,14";
       width               = 320;
       icons               = true;
+      "on-button-left"    = "invoke";
+      "on-button-right"   = "dismiss";
+
+      "urgency=critical" = {
+        "default-timeout" = 0;
+        "border-color"    = "#ffaa00";
+      };
     };
   };
 }
