cfb1b92fda116bff48ea63fb2ad71a5ba7c9172b
Yago Lima Lins <yago.lima.lins@protonmail.com>
2026-07-24 10:37:36 -0300

    feat: add input remapper bt preset

--- modules/home/input-remapper/default.nix
+++ modules/home/input-remapper/default.nix
@@ -26,11 +26,231 @@ in
         version = "2.2.0";
         autoload = {
           "Compx 2.4G Wireless Receiver" = "Redragon Fizz Pro";
+          "BT5.0 KB Keyboard" = "Redragon Fizz Pro";
         };
       };
     };
 
     xdg.configFile."input-remapper-2/presets/Compx 2.4G Wireless Receiver/Redragon Fizz Pro.json" = {
+      force = true;
+      text = builtins.toJSON [
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 97;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 17;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Up";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 97;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 30;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Left";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 97;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 31;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Down";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 97;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 32;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Right";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 97;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 1;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "apostrophe";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 97;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 42;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 1;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Shift_L + apostrophe";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 54;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 42;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 1;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Shift_L + apostrophe";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 54;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 1;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "apostrophe";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 54;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 17;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Up";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 54;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 30;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Left";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 54;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 31;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Down";
+          mapping_type = "key_macro";
+        }
+        {
+          input_combination = [
+            {
+              type = 1;
+              code = 54;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+            {
+              type = 1;
+              code = 32;
+              origin_hash = "16449b01c96d206924e4566166d681f8";
+            }
+          ];
+          target_uinput = "keyboard";
+          output_symbol = "Right";
+          mapping_type = "key_macro";
+        }
+      ];
+    };
+    xdg.configFile."input-remapper-2/presets/BT5.0 KB Keyboard/Redragon Fizz Pro.json" = {
       force = true;
       text = builtins.toJSON [
         {
