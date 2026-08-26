c1a59371a372278f210ba25bf2621f8fb6c6fa0f
Yago Lima Lins <yago.lima.lins@protonmail.com>
2026-08-26 17:14:09 -0300

    feat: change cpu governor on ac to performance

--- systems/x86_64-linux/thinkpad/custom.nix
+++ systems/x86_64-linux/thinkpad/custom.nix
@@ -18,7 +18,7 @@
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_AC = 0;
